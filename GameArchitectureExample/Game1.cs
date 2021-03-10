72b00672aaf9a5f71874e854898af80cebed1fdf
Cole Willenbring <cjwillenbring@users.campus>
2021-03-10 15:19:52 -0600

    Small Change

--- GameArchitectureExample/Game1.cs
+++ GameArchitectureExample/Game1.cs
@@ -46,9 +46,6 @@ namespace GameArchitectureExample
 
         protected override void Update(GameTime gameTime)
         {
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
-                Exit();
-
             base.Update(gameTime);
         }
 
